7cd25f9e0a21af7bd0600eeceae1cf9764993f2c
annafiega <annafiega@gmail.com>
2018-03-09 10:58:48 +0100

    14.4

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+
+    @RequestMapping (method= RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTasks(){
+      return new ArrayList<>();
+    }
+
+    @RequestMapping (method= RequestMethod.GET, value = "getTask")
+    public TaskDto getTask(String taskId){
+        return new TaskDto((long)1,"test title","test_content");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    public void deleteTask(String taskId){
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask(TaskDto task){
+        return new TaskDto((long)1,"Edited test title", "Test content");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask(TaskDto task){
+    }
+}
