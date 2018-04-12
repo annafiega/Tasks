55b3465f6e040313ad0a6ee0dc96b750b9a3ebf1
annafiega <annafiega@gmail.com>
2018-04-12 20:14:23 +0200

    17.4

---
+++ showtasks.bat
@@ -0,0 +1,20 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto runbrowser
+echo.
+echo rancrud.bat has errors - breaking work
+goto fail
+
+:runbrowser
+
+start "start" "http://localhost:8080/crud/v1/task/getTasks"
+if "%ERRORLEVEL%" == "0" goto end
+echo Website cannot be open
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
