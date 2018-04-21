862b77af06320e5be18ae8af7abc270cdb62220f
annafiega <annafiega@gmail.com>
2018-04-21 19:19:28 +0200

    First attempt to Heroku

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -22,18 +22,7 @@ public class TrelloController {
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards(){
 
-       /*List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-      //trelloBoards.stream().filter(trelloBoardDto ->trelloBoardDto.getName().contains("Kodilla")).forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
-        trelloBoards.forEach(trelloBoardDto -> {
 
-            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-
-            System.out.println("This board contains lists: ");
-
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-
-        });*/
        return trelloClient.getTrelloBoards();
     }
 
--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -20,7 +20,6 @@ public class CreatedTrelloCard {
 
     @JsonProperty("shortUrl")
     private String shortUrl;
-
    /* @JsonProperty ("badges")
     private BadgesDto bagdes;*/
 }
