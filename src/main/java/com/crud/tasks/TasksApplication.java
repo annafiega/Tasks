842fb65c6c744b711813d25507c68239c5135aba
annafiega <annafiega@gmail.com>
2018-04-23 13:00:03 +0200

    19.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,15 +6,20 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
+//@SpringBootApplication
+//public class TasksApplication extends SpringBootServletInitializer {
+//	public static void main(String[] args) {
+//		SpringApplication.run(TasksApplication.class, args);
+//	}
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(TasksApplication.class);
+//    }
+//}
 @SpringBootApplication
-public class TasksApplication {//extends SpringBootServletInitializer{
-
+public class TasksApplication {
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
-	/*@Override
-	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
-		return application.sources(TasksApplication.class);
-	}*/
-
 }
